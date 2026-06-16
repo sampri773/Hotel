7126dde04524f5d30051b0977799229f01a319d4
RAZAFINDRAKOTO Andraina <hei.andraina.6@gmail.com>
2026-06-16 16:37:42 +0300

    modif des attributs du classe room

--- src/main/java/com/hotel/Bed.java
+++ src/main/java/com/hotel/Bed.java
@@ -1,4 +1,4 @@
 package com.hotel;
 
-public class Lit {
+public class Bed {
 }
--- src/main/java/com/hotel/Room.java
+++ src/main/java/com/hotel/Room.java
@@ -3,10 +3,12 @@ package com.hotel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 public class Room {
     private int id;
     private Client client;
-    private Lit lit;
+    private List<Bed> bed;
 }
