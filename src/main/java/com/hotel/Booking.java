bfff4d1d6597aca31343b300955ab169fdd41792
hasi-nah <hasina.hei@gmail>
2026-06-18 15:06:52 +0300

    classe Booking

---
+++ src/main/java/com/hotel/Booking.java
@@ -0,0 +1,14 @@
+package com.hotel;
+
+
+import java.time.LocalDate;
+
+public class Booking {
+    private String id;
+    private String customerName;
+    private Room room;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int guestCount ;
+
+}
--- src/main/java/com/hotel/Reservation.java
+++ src/main/java/com/hotel/Reservation.java
@@ -1,5 +1,16 @@
 package com.hotel;
 
-public class Reservation {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Reservation {
+    private final  List<Room> rooms = new ArrayList<>();
+    private final  List<Booking> bookings = new ArrayList<>();
 }
