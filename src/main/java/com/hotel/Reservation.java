cc5b1e8298de33ab1884620bd6ac65606ab484da
hasi-nah <hasina.hei@gmail>
2026-06-18 16:03:17 +0300

    petit changement

--- src/main/java/com/hotel/Reservation.java
+++ src/main/java/com/hotel/Reservation.java
@@ -1,16 +1,3 @@
 package com.hotel;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class Reservation {
-    private final  List<Room> rooms = new ArrayList<>();
-    private final  List<Booking> bookings = new ArrayList<>();
-}
