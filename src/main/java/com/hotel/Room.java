31f8fea0d66f610cc4f57d2a3959fd8232df4abb
RAZAFINDRAKOTO Andraina <hei.andraina.6@gmail.com>
2026-06-28 14:06:02 +0300

    création d'enum et méthodes StatutChambre

--- src/main/java/com/hotel/Room.java
+++ src/main/java/com/hotel/Room.java
@@ -3,6 +3,7 @@ package com.hotel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
@@ -14,4 +15,23 @@ public class Room {
     private List<Bed> bed;
     private PlaceNumber capacity;
     private List<Reservation> reservations;
+
+    public RoomStatus getRoomStatus(LocalDate date) {
+        for (Reservation r : reservations) {
+            if (!date.isBefore(r.getStartDate()) && !date.isAfter(r.getEndDate())) {
+                return RoomStatus.OCCUPIED;
+            }
+        }
+        return RoomStatus.FREE;
+    }
+
+    public boolean isOverlap(LocalDate start, LocalDate end) {
+        for(Reservation r : reservations) {
+            boolean overlap = !end.isBefore(r.getStartDate()) && !end.isAfter(r.getEndDate());
+            if (overlap) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ src/main/java/com/hotel/RoomStatus.java
@@ -0,0 +1,5 @@
+package com.hotel;
+
+public enum RoomStatus {
+    FREE, OCCUPIED, RESERVED
+}
